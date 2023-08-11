9f2c1a78539399a650aee83bcbc2963ae9af8d5b
Bidyut Das <das.bidyut12345@gmail.com>
2023-08-11 11:58:48 +0530

    custom button upgrade and added new features

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -3,29 +3,51 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
+  final double radius;
+  final EdgeInsets margin;
+  final EdgeInsets? padding;
+  final double fontSize;
+  final Color? backcolor;
+  final Color? forecolor;
+  final Alignment alignment;
   // final Color color;
   const CustomButton({
     Key? key,
     required this.text,
     required this.onPressed,
+    this.radius = 15,
+    this.margin = EdgeInsets.zero,
+    this.padding,
+    this.fontSize = 14,
+    this.backcolor,
+    this.forecolor,
+    this.alignment = Alignment.center,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-          // foregroundColor: Colors.indigo[900],
-          backgroundColor: const Color.fromARGB(255, 128, 136, 214),
+    return Padding(
+      padding: margin,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          padding: padding,
+          backgroundColor: backcolor ?? const Color.fromARGB(255, 128, 136, 214),
           minimumSize: const Size(double.infinity, 60),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
-          // shape:
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
+          alignment: alignment,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: forecolor ?? Color.fromARGB(255, 228, 228, 228),
+            fontSize: fontSize,
           ),
-      child: Text(
-        text,
-        style: const TextStyle(
-          color: Color.fromARGB(255, 228, 228, 228),
-          fontSize: 14,
+          textAlign: [Alignment.topLeft, Alignment.bottomLeft, Alignment.centerLeft].contains(alignment)
+              ? TextAlign.left
+              : [Alignment.center, Alignment.topCenter, Alignment.bottomCenter].contains(alignment)
+                  ? TextAlign.center
+                  : TextAlign.right,
         ),
       ),
     );
