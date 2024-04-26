8265dde61427b464c0b172a70e0d01562400c3e2
Bidyut Das <das.bidyut12345@gmail.com>
2024-04-26 16:32:58 +0530

    New updates

--- lib/src/loading_view.dart
+++ lib/src/loading_view.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../common_widgets.dart';
 
 class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({Key? key, required this.child, this.fit = StackFit.expand}) : super(key: key);
+  const LoadingWidget({Key? key, required this.child, this.fit = StackFit.loose}) : super(key: key);
   final Widget child;
   final StackFit fit;
   @override
@@ -17,40 +16,38 @@ class LoadingWidget extends StatelessWidget {
         LoadingController.instance!.isLoading
             ? Container(
                 color: Colors.black.withOpacity(0.3),
-                child: Center(
-                  child: AlertDialog(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                    clipBehavior: Clip.antiAlias,
-                    content: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const SizedBox(
-                            height: 100,
-                            child: Center(
-                              child: SizedBox(
-                                height: 60,
-                                width: 60,
-                                child: CircularProgressIndicator(
-                                  strokeWidth: 8,
-                                ),
+                child: AlertDialog(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  clipBehavior: Clip.antiAlias,
+                  content: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: SizedBox(
+                              height: 60,
+                              width: 60,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 8,
                               ),
                             ),
                           ),
-                          if (LoadingController.instance!.loadingText != "") ...[
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Text(
-                              LoadingController.instance!.loadingText,
-                              style: const TextStyle(fontWeight: FontWeight.bold),
-                            )
-                          ]
-                        ],
-                      ),
+                        ),
+                        if (LoadingController.instance!.loadingText != "") ...[
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            LoadingController.instance!.loadingText,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ]
+                      ],
                     ),
                   ),
                 ),
