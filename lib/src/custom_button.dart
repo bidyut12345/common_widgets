0638b0a85c74dadcfa4b0d6fa385e5c30cb57c62
Bidyut Das <das.bidyut12345@gmail.com>
2024-10-14 22:42:35 +0530

    New updates

--- lib/src/custom_button.dart
+++ lib/src/custom_button.dart
@@ -10,6 +10,8 @@ class CustomButton extends StatelessWidget {
   final Color? backcolor;
   final Color? forecolor;
   final Alignment alignment;
+  final double? height;
+  final double? width;
   // final Color color;
   const CustomButton({
     Key? key,
@@ -21,6 +23,8 @@ class CustomButton extends StatelessWidget {
     this.fontSize = 14,
     this.backcolor,
     this.forecolor,
+    this.height,
+    this.width,
     this.alignment = Alignment.center,
   }) : super(key: key);
 
@@ -28,30 +32,31 @@ class CustomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: margin,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          padding: padding,
-          backgroundColor: backcolor ??
-              (Theme.of(context).brightness == Brightness.dark
-                  ? Color.fromARGB(255, 53, 56, 82)
-                  : const Color.fromARGB(255, 128, 136, 214)),
-          minimumSize: const Size(double.infinity, 60),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
-          alignment: alignment,
-        ),
-        child: Text(
-          text,
-          style: TextStyle(
-            color: forecolor ??
-                (Theme.of(context).brightness == Brightness.dark ? Colors.white : Color.fromARGB(255, 228, 228, 228)),
-            fontSize: fontSize,
+      child: SizedBox(
+        height: height,
+        width: width,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            padding: padding,
+            backgroundColor:
+                backcolor ?? (Theme.of(context).brightness == Brightness.dark ? Color.fromARGB(255, 53, 56, 82) : const Color.fromARGB(255, 128, 136, 214)),
+            minimumSize: const Size(double.infinity, 60),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
+            alignment: alignment,
           ),
-          textAlign: [Alignment.topLeft, Alignment.bottomLeft, Alignment.centerLeft].contains(alignment)
-              ? TextAlign.left
-              : [Alignment.center, Alignment.topCenter, Alignment.bottomCenter].contains(alignment)
-                  ? TextAlign.center
-                  : TextAlign.right,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: forecolor ?? (Theme.of(context).brightness == Brightness.dark ? Colors.white : Color.fromARGB(255, 228, 228, 228)),
+              fontSize: fontSize,
+            ),
+            textAlign: [Alignment.topLeft, Alignment.bottomLeft, Alignment.centerLeft].contains(alignment)
+                ? TextAlign.left
+                : [Alignment.center, Alignment.topCenter, Alignment.bottomCenter].contains(alignment)
+                    ? TextAlign.center
+                    : TextAlign.right,
+          ),
         ),
       ),
     );
--- lib/src/scale_widget.dart
+++ lib/src/scale_widget.dart
@@ -2,16 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 class ScaleContainer extends StatefulWidget {
-  const ScaleContainer(
-      {Key? key,
-      required this.child,
-      required this.appScale,
-      this.isPadding = true})
-      : super(key: key);
+  const ScaleContainer({
+    Key? key,
+    required this.child,
+    required this.appScale,
+    this.isPadding = true,
+    this.resizeToAvoidBottomInset = true,
+  }) : super(key: key);
 
   final Widget child;
   final double appScale;
   final bool isPadding;
+  final bool resizeToAvoidBottomInset;
   @override
   State<ScaleContainer> createState() => _ScaleContainerState();
 }
@@ -19,32 +21,27 @@ class ScaleContainer extends StatefulWidget {
 class _ScaleContainerState extends State<ScaleContainer> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return Container(
       child: kIsWeb
           ? widget.child
-          : LayoutBuilder(
-              builder: (context, constraint) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                    bottom: widget.isPadding
-                        ? MediaQuery.of(context).viewInsets.bottom
-                        : MediaQuery.of(context).viewInsets.bottom *
-                            (1 - widget.appScale),
+          : Padding(
+              padding: widget.resizeToAvoidBottomInset
+                  ? EdgeInsets.only(
+                      bottom: widget.isPadding ? MediaQuery.of(context).viewInsets.bottom : MediaQuery.of(context).viewInsets.bottom * (1 - widget.appScale),
+                    )
+                  : EdgeInsets.zero,
+              child: FractionallySizedBox(
+                widthFactor: 1 / widget.appScale,
+                heightFactor: 1 / widget.appScale,
+                child: Transform.scale(
+                  scale: widget.appScale,
+                  child: LayoutBuilder(
+                    builder: (layoutcontext, layoutconstraint) {
+                      return widget.child;
+                    },
                   ),
-                  child: FractionallySizedBox(
-                    widthFactor: 1 / widget.appScale,
-                    heightFactor: 1 / widget.appScale,
-                    child: Transform.scale(
-                      scale: widget.appScale,
-                      child: LayoutBuilder(
-                        builder: (layoutcontext, layoutconstraint) {
-                          return widget.child;
-                        },
-                      ),
-                    ),
-                  ),
-                );
-              },
+                ),
+              ),
             ),
     );
   }
