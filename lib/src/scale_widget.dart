ab0672852f84e6b5c5b68c112f77fc62108e67ee
Bidyut Das <das.bidyut12345@gmail.com>
2026-04-14 22:27:04 +0530

    New updates

--- lib/src/scale_widget.dart
+++ lib/src/scale_widget.dart
@@ -2,13 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
 class ScaleContainer extends StatefulWidget {
-  const ScaleContainer({
-    Key? key,
-    required this.child,
-    required this.appScale,
-    this.isPadding = true,
-    this.resizeToAvoidBottomInset = true,
-  }) : super(key: key);
+  const ScaleContainer({Key? key, required this.child, required this.appScale, this.isPadding = true, this.resizeToAvoidBottomInset = true}) : super(key: key);
 
   final Widget child;
   final double appScale;
@@ -21,28 +15,31 @@ class ScaleContainer extends StatefulWidget {
 class _ScaleContainerState extends State<ScaleContainer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: kIsWeb
-          ? widget.child
-          : Padding(
-              padding: widget.resizeToAvoidBottomInset
-                  ? EdgeInsets.only(
-                      bottom: widget.isPadding ? MediaQuery.of(context).viewInsets.bottom : MediaQuery.of(context).viewInsets.bottom * (1 - widget.appScale),
-                    )
-                  : EdgeInsets.zero,
-              child: FractionallySizedBox(
-                widthFactor: 1 / widget.appScale,
-                heightFactor: 1 / widget.appScale,
-                child: Transform.scale(
-                  scale: widget.appScale,
-                  child: LayoutBuilder(
-                    builder: (layoutcontext, layoutconstraint) {
-                      return widget.child;
-                    },
+    return widget.appScale == 1.0
+        ? widget.child
+        : Container(
+            child: kIsWeb
+                ? widget.child
+                : Padding(
+                    padding: widget.resizeToAvoidBottomInset
+                        ? EdgeInsets.only(
+                            bottom:
+                                widget.isPadding ? MediaQuery.of(context).viewInsets.bottom : MediaQuery.of(context).viewInsets.bottom * (1 - widget.appScale),
+                          )
+                        : EdgeInsets.zero,
+                    child: FractionallySizedBox(
+                      widthFactor: 1 / widget.appScale,
+                      heightFactor: 1 / widget.appScale,
+                      child: Transform.scale(
+                        scale: widget.appScale,
+                        child: LayoutBuilder(
+                          builder: (layoutcontext, layoutconstraint) {
+                            return widget.child;
+                          },
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
-    );
+          );
   }
 }
